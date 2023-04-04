acb62e02f4af828d6b94668f66891b46d5b37285
Pazik <pazdavid2008@gmail.com>
2023-04-04 20:54:25 +0300

    Added implementations for methods in mario.py

--- mario.py
+++ mario.py
@@ -6,3 +6,22 @@ class Mario(Character):
     def __init__(self, pos, image):
         self.x, self.y = pos[0], pos[1]
         self.image = image
+
+    def left(self):
+        pass
+
+    def right(self):
+        pass
+
+    def jump(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def die(self):
+        pass
+
+    def speak(self, what_to_say):
+        pass
+
