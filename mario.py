817a0aa80c1fb7bc00a4725e123c69162f478b3a
ItayAviram <97438773+ItayAviram@users.noreply.github.com>
2023-04-04 19:26:54 +0300

    Update mario.py

--- mario.py
+++ mario.py
@@ -2,7 +2,7 @@ from character import Character
 import pygame
 
 
-class Princess(Character):
+class Mario(Character):
     def __init__(self, pos, image, leaves, throws=3, health=3):
         self.x, self.y = pos[0], pos[1]
         self.image = image
