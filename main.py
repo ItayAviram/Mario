4333f9d30784657a0b4c9f455e1d790a5607fab8
ItayAviram <itayaviram2007@gmail.com>
2023-04-21 15:15:02 +0300

    Update

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from mario import Mario
 from mushroom import mushroom
 from princess import Princess
 from enemy import Enemy
+from python_snake import python_snake
+from carnivorous_plant import carnivorous_plant
+from button import button
+from acorn import acorn
+from banana_peel import banana_peel
 
 pygame.init()
 width, height = (900, 600)
@@ -17,6 +22,11 @@ p = Princess((100, 100), pygame.Surface((10, 50)), 0)
 c = checkpoint((150, 100), pygame.Surface((10, 80)), 0)
 m = mushroom((110, 100), pygame.Surface((10, 10)), 0)
 ma = Mario((200, 100), pygame.Surface((10, 50)), 0)
+py = python_snake((160, 100), pygame.Surface((10, 5)), 0)
+ca = carnivorous_plant((170, 100), pygame.Surface((20, 80)), 0)
+b = button((180, 100), pygame.Surface((15, 15)), 0)
+a = acorn((190, 100), pygame.Surface((5, 5)), 0)
+ba = banana_peel((200, 100), pygame.Surface((5, 5)), 0)
 
 enemyimage = pygame.image.load(r'images\enemy.png') # type: pygame.Surface
 
@@ -43,6 +53,16 @@ def main():
         m.draw(screen)
 
         ma.draw(screen)
+
+        py.draw(screen)
+
+        ca.draw(screen)
+
+        b.draw(screen)
+
+        a.draw(screen)
+
+        ba.draw(screen)
 
         e.draw(screen)
         e.update()
