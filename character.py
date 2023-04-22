bc8385d50cfe2f9c15bfcd96cbcef47c2ccf52a3
Pazik <pazdavid2008@gmail.com>
2023-04-22 11:03:19 +0300

    Update Abstract classes, Enemy.py

--- character.py
+++ character.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 from movement import Movement
-from movement import AutoMovEnemy
 import pygame
 
 
@@ -45,59 +44,15 @@ class Character(Movement):
         :return: None
         """
 
-
-class CharacterEnemy(AutoMovEnemy):
-    # Character is abstract because it inherits from Movement which is also abstract
-    """
-    This is an abstract class that represents the characters in the game
-    """
-
-    # noinspection PyMissingConstructor
-    @abstractmethod
-    def __init__(self, pos, image, leaves, throws=3, health=3):
-        """
-        :param tuple pos: character position (x, y)
-        :param pygame.Surface image: image of the character
-        :param int leaves: ranges 0 - 4, is a counter for the leaves the character picked up
-        :param int throws: ranges 0 - 3, is a counter for how many throws does the character have
-        :param int health: ranges 1 - 3, is the number of hearts the character has until it dies
-        (health != 0)
-        """
-        pass
-
-    @abstractmethod
-    def die(self):
-        pass
-
-    @abstractmethod
-    def speak(self, what_to_say):
-        """
-        Creates a speech bubble with given text
-
-        :param str what_to_say: the content of the speech bubble
-        """
-        pass
-
     @abstractmethod
     def get_rect(self):
         """
         this method returns the character's rect in the form of pygame.Surface
+        (used for collisions)
         :return: self.rect
         """
         pass
 
 
-    @abstractmethod
-    def draw(self, surface):
-        """
-        draw the image on the screen.
-
-        :param pygame.Surface surface: the screen
-        :return: None
-        """
-
 if __name__ == "__main__":
     print(Character.__mro__)
-
-if __name__ == "__main__":
-    print(CharacterEnemy.__mro__)
--- enemy.py
+++ enemy.py
@@ -4,20 +4,20 @@ import pygame
 
 class Enemy(Character):
     # noinspection PyMissingConstructor
-    def __init__(self, pos, size, image, leaves, throws=3, health=3):
+    def __init__(self, pos: tuple, size: tuple, image: pygame.Surface, leaves: int, throws=3, health=3):
         self.x, self.y = pos[0], pos[1]  # (x, y)
+
         self.image = image
-
         self.image = pygame.transform.scale(image, size)
-
         self.rect = pygame.Rect(self.x, self.y, size[0], size[1])
+
         self.leaves = leaves
         self.throws = throws
         self.health = health
         # movement:
         #   constants:
-        self.xvel = 8
-        self.jump_height = 8
+        self.xvel = 2
+        self.jump_height = 0
         self.gravity = 0.25
         #   changeables:
         self.direction = 0  # direction of x
@@ -30,7 +30,8 @@ class Enemy(Character):
         self.y += self.yvel
 
     def jump(self):
-        self.yvel = self.jump_height * (-1)
+        pass
+        # enemies can't jump
 
     def stop(self):
         self.yvel = 0
@@ -50,22 +51,7 @@ class Enemy(Character):
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
 
-    def get_input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_d]:
-            self.direction = 1
-        elif keys[pygame.K_a]:
-            self.direction = -1
-        else:
-            self.direction = 0
-        if keys[pygame.K_w]:
-            self.jump()
-        if keys[pygame.K_SPACE]:
-            self.stop()
-
-
     def update(self):
-        self.get_input()
         self.move_x()
         self.move_y()
         self.accelerate()
